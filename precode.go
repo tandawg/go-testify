fab8ea8c028aa76367c990dddb030eb83d6097e0
Tatiana Fedorova <tandawg@yandex.ru>
2024-10-02 00:27:36 +0300

    second commit

--- precode.go
+++ precode.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-    "net/http"
-    "net/http/httptest"
+	"net/http"
     "strconv"
-    "strings"
-    "testing"
+	"strings"
 )
 
 var cafeList = map[string][]string{
@@ -46,13 +44,10 @@ func mainHandle(w http.ResponseWriter, req *http.Request) {
     w.Write([]byte(answer))
 }
 
-func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
-    totalCount := 4
-    req := ... // здесь нужно создать запрос к сервису
-
-    responseRecorder := httptest.NewRecorder()
-    handler := http.HandlerFunc(mainHandle)
-    handler.ServeHTTP(responseRecorder, req)
-
-    // здесь нужно добавить необходимые проверки
+func main() {
+	http.HandleFunc("/cafe", mainHandle)
+	err := http.ListenAndServe(":8080", nil)
+    if err != nil {
+        panic(err)
+    }
 }
---
+++ precode_test.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestMainHandlerWhenOk(t *testing.T) {
+	req := httptest.NewRequest("GET", "/cafe?count=2&city=moscow", nil)
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	assert.Equal(t, http.StatusOK, responseRecorder.Code)
+
+	assert.NotEmpty(t, responseRecorder.Body)
+
+	body := responseRecorder.Body.String()
+	cafes := strings.Split(body, ",")
+	assert.Len(t, cafes, 2)
+}
+
+func TestMainHandlerWhenMissingCount(t *testing.T) {
+	req := httptest.NewRequest("GET", "/cafe?city=moscow", nil)
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	assert.Equal(t, http.StatusBadRequest, responseRecorder.Code)
+
+	expected := "count missing"
+	assert.Equal(t, expected, responseRecorder.Body.String())
+}
+
+func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
+	totalCount := 4
+	req := httptest.NewRequest("GET", "/cafe?count=10&city=moscow", nil)
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	require.Equal(t, http.StatusOK, responseRecorder.Code)
+
+	assert.NotEmpty(t, responseRecorder.Body)
+
+	body := responseRecorder.Body.String()
+	cafes := strings.Split(body, ",")
+	assert.Len(t, cafes, totalCount)
+}
